4ec6e8c6005cdceb6af5cf4894f337751bbe0c6c
i-xi-dev <naoyuki.isono@gmail.com>
2023-10-15 00:56:47 +0900

    in development

--- tests/duration.test.ts
+++ tests/duration.test.ts
@@ -7,6 +7,21 @@ Deno.test("Duration.ofSeconds(number)", () => {
   assertStrictEquals(Duration.ofSeconds(0), 0);
   assertStrictEquals(Duration.ofSeconds(1), 1000);
   assertStrictEquals(Duration.ofSeconds(1.5), 1500);
+
+  assertThrows(
+    () => {
+      Duration.ofSeconds(Number.NaN);
+    },
+    TypeError,
+    "seconds",
+  );
+  assertThrows(
+    () => {
+      Duration.ofSeconds("1" as unknown as number);
+    },
+    TypeError,
+    "seconds",
+  );
 });
 
 Deno.test("Duration.toSeconds(number)", () => {
@@ -15,4 +30,66 @@ Deno.test("Duration.toSeconds(number)", () => {
   assertStrictEquals(Duration.toSeconds(0), 0);
   assertStrictEquals(Duration.toSeconds(1000), 1);
   assertStrictEquals(Duration.toSeconds(1500), 1.5);
+
+  assertThrows(
+    () => {
+      Duration.toSeconds(Number.NaN);
+    },
+    TypeError,
+    "milliseconds",
+  );
+  assertThrows(
+    () => {
+      Duration.toSeconds("1" as unknown as number);
+    },
+    TypeError,
+    "milliseconds",
+  );
 });
+
+Deno.test("Duration.ofMinutes(number)", () => {
+  assertStrictEquals(Duration.ofMinutes(-1), -60000);
+  assertStrictEquals(Duration.ofMinutes(-0), 0);
+  assertStrictEquals(Duration.ofMinutes(0), 0);
+  assertStrictEquals(Duration.ofMinutes(1), 60000);
+  assertStrictEquals(Duration.ofMinutes(1.5), 90000);
+
+  assertThrows(
+    () => {
+      Duration.ofMinutes(Number.NaN);
+    },
+    TypeError,
+    "minutes",
+  );
+  assertThrows(
+    () => {
+      Duration.ofMinutes("1" as unknown as number);
+    },
+    TypeError,
+    "minutes",
+  );
+});
+
+Deno.test("Duration.toMinutes(number)", () => {
+  assertStrictEquals(Duration.toMinutes(-60000), -1);
+  assertStrictEquals(Duration.toMinutes(-0), 0);
+  assertStrictEquals(Duration.toMinutes(0), 0);
+  assertStrictEquals(Duration.toMinutes(60000), 1);
+  assertStrictEquals(Duration.toMinutes(90000), 1.5);
+
+  assertThrows(
+    () => {
+      Duration.toMinutes(Number.NaN);
+    },
+    TypeError,
+    "milliseconds",
+  );
+  assertThrows(
+    () => {
+      Duration.toMinutes("1" as unknown as number);
+    },
+    TypeError,
+    "milliseconds",
+  );
+});
+
