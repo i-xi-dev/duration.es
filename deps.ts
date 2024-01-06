470e04172b2f8fcfe67b9423b5ba835baf1be789
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-06 22:55:01 +0900

    1.0.4

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { SafeInteger } from "https://raw.githubusercontent.com/i-xi-dev/number.es/5.4.0/mod.ts";
-export * as StringEx from "https://raw.githubusercontent.com/i-xi-dev/string.es/4.0.0/mod.ts";
+export { SafeInteger } from "https://raw.githubusercontent.com/i-xi-dev/number.es/5.4.1/mod.ts";
+export * as StringEx from "https://raw.githubusercontent.com/i-xi-dev/string.es/4.0.1/mod.ts";
