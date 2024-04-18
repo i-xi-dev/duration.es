d6fa217050d4a9a3815f8a5a15fb6bfa5c47fc1d
i-xi-dev <naoyuki.isono@gmail.com>
2024-04-19 00:12:23 +0900

    1.0.13

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { NumberEx } from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.3.0/mod.ts";
-export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.1/mod.ts";
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.4/mod.ts";
+export { NumberEx } from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.0/mod.ts";
+export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.2/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.5/mod.ts";
