6539e6abd50581ccd11817a1e1e21bc3cf91ce40
i-xi-dev <naoyuki.isono@gmail.com>
2024-06-17 01:48:15 +0900

    2.1.3

--- deps.ts
+++ deps.ts
@@ -2,6 +2,6 @@ export {
   NumberEx,
   RoundingMode,
   SafeInteger,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.1/mod.ts";
-export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.3/mod.ts";
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.0/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.1.0/mod.ts";
+export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.4/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.1/mod.ts";
